790a067703125f887c89a6c32734e030b45cb942
Pablo Fabbian <pablo.fabbian@gmail.com>
2024-03-16 01:09:45 -0300

    HeroHeader Compleition + Mobile Adjustability
    Se definió la personalidad de la Landing (nuevo fondo y detalles visuales) y se logró una buena visualización en dispositivos moviles.

--- src/components/ANavbar/NavBar.jsx
+++ src/components/ANavbar/NavBar.jsx
@@ -3,18 +3,20 @@ import React from 'react'
 function NavBar() {
     return (
         <header className="text-gray-400 bg-gray-900 body-font">
-            <div className="ml-10 mx-auto flex flex-wrap p-4 items-center">
+            <div className="ml-10 mx-auto flex flex-wrap p-4 items-center justify-center">
                 <nav className="flex lg:w-2/5 flex-wrap items-center text-s md:ml-auto ">
-                    <a className="mr-5 hover:text-white">Inicio</a>
-                    <a className="mr-5 hover:text-white">Shop</a>
-                    <a className="mr-5 hover:text-white">Como comprar</a>
-                    <a className="mr-5 hover:text-white">Quienes Somos</a>
+                    <a className="mr-8 cursor-pointer hover:text-white">Inicio</a>
+                    <a className="mr-8 cursor-pointer hover:text-white">Shop</a>
+                    <a className="mr-8 cursor-pointer hover:text-white">Como comprar</a>
+                    <a className="mr-8 cursor-pointer hover:text-white">Quienes Somos</a>
                 </nav>
-                <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
+                <a className="flex justify-center items-center order-first lg:order-none lg:w-1/5 title-font font-medium text-white lg:items-center lg:justify-center mb-4 md:mb-0">
+                    <h1 className="mr-4">Vooid</h1>
                     <img src="/Logo2.webp" className="h-12" alt="Void Indum." />
+                    <h1 className="ml-4">Indum.</h1>
                 </a>
                 <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-                    <img src="/Cart.png" className="h-7 mr-10" alt="Carrito" />
+                    <img src="/Cart.png" className="h-7 mr-10 mt-4 xl:mt-0 lg:mt-0 md:mt-0" alt="Carrito" />
                 </div>
             </div>
         </header>
--- src/components/BHeroHeaderSection/HeroHeaderSection.jsx
+++ src/components/BHeroHeaderSection/HeroHeaderSection.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 
 const HeroHeaderSection = () => {
     return (
-        <div className="bg-cover bg-center h-screen flex items-center filter brightness-75" style={{backgroundImage: "url('Gatobg.webp')"}}>
+        <div className="bg-cover bg-center h-screen flex items-center filter" style={{backgroundImage: "url('Gatobg32.jpg')"}}>
             <div className="container mx-auto px-4">
-                <div className="lg:flex-grow md:w-4/6 -ml-20 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-white">
+                <div className="flex flex-col items-left text-center md:w-4/6 -mt-40 lg:-mt-16 lg:-ml-40 md:ml-20 md:items-start md:text-left mb-16 md:mb-0">
+                    <h1 className="shadow-behind mb-4 font-bold text-white leading-tight text-4xl md:text-5xl">
                         Descubrí los Últimos Diseños
-                        <br />de Indumentaria Oversize
+                        <br />de Indumentaria
+                        <br />Oversize
                     </h1>
                     <p className="mb-8 leading-relaxed text-white text-pretty">
                         Compra ahora diseños exclusivos o personalizados que te hagan único.
                         <br className="hidden lg:inline-block"/>Calidad, color y estampados que se ajustan a tu estilo.
                     </p>
                     <div className="flex justify-center">
-                        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Shop</button>
-                        <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Learn More</button>
+                        <button className="inline-flex text-white bg-indigo-600 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Shop</button>
+                        <button className="ml-4 inline-flex text-indigo-600 bg-slate-100 border-0 py-2 px-6 focus:outline-none hover:bg-slate-200 hover:text-white rounded text-lg">Learn More</button>
                     </div>
                 </div>
             </div>
